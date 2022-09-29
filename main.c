e92699cc7867579c49d29f6b1c4179e0a04c6029
lipo <85422792+thiliporace@users.noreply.github.com>
2022-09-29 00:31:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int x = 10;
+  int y = 25;
+  int z = x + y;
+  printf("Hello World = %i\n", z);
+
+  /* int c = 0;
+  int i = c++;
+  int j = ++c;
+
+  printf("c = %d, i = %d, j = %d", c, i, j);
+  */
+}
